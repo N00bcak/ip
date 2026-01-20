e629cdea6a591917a912306c0bd9d9b7ab1d105a
N00bcak <depictiger@gmail.com>
2026-01-20 22:21:34 +0800

    Complete Level 0

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Dooki User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Dooki.java
@@ -0,0 +1,16 @@
+/**
+ * Entrypoint to chatting with Dooki
+ */
+
+public class Dooki {
+    // Adapted output format from https://nus-cs2103-ay2526-s2.github.io/website/schedule/week2/project.html
+    private static final String LONG_LINE = "_".repeat(30);
+    public static void main(String[] args) {
+        System.out.println(LONG_LINE);
+        System.out.println("Hello! I'm Dooki");
+        System.out.println("What can I do for you?");
+        System.out.println(LONG_LINE);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LONG_LINE);
+    }
+}
